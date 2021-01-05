ce2a686f47816efb6db56f46aa73dfc8cba2c91a
FannyHunin <fanny.hunin.an123@gmail.com>
2021-01-05 11:51:31 +0100

    partie home laravelisée

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,49 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset("css/app.css")}}">
-    <title>Document</title>
-</head>
+    <head>
+        <title>@yield('pageTitle')</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Labs - Design Studio">
+        <meta name="keywords" content="lab, onepage, creative, html">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="shortcut icon"/>
+    
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700|Roboto:300,400,700" rel="stylesheet">
+    
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}"/>
+        <link rel="stylesheet" href="{{asset("css/font-awesome.min.css")}}"/>
+        <link rel="stylesheet" href="{{asset("css/flaticon.css")}}"/>
+        <link rel="stylesheet" href="{{asset("css/magnific-popup.css")}}"/>
+        <link rel="stylesheet" href="{{asset("css/owl.carousel.css")}}"/>
+        <link rel="stylesheet" href="{{asset("css/style.css")}}"/>
+    
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    
+    </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-      <script src="{{asset("js/app.js")}}"></script>
+    @include('partials.navbar')
+    @yield('content')
+
+    <!--====== Javascripts & Jquery ======-->
+	<script src="{{asset("js/jquery-2.1.4.min.js")}}"></script>
+	<script src="{{asset("js/bootstrap.min.js")}}"></script>
+	<script src="{{asset("js/magnific-popup.min.js")}}"></script>
+	<script src="{{asset("js/owl.carousel.min.js")}}"></script>
+	<script src="{{asset("js/circle-progress.min.js")}}"></script>
+	<script src="{{asset("js/main.js")}}"></script>
 </body>
 </html>
